f86417279da38bf29fd08d1bcb24cf79911b985d
Shota Ikuno <171012789+ShoIku@users.noreply.github.com>
2025-04-04 12:18:52 +0900

    memoListItems Done

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,18 +3,19 @@ import { View, Text, StyleSheet } from 'react-native'
 const Index = (): JSX.Element => {
     return (
         <View style={styles.container}>
-            <View>
-                <View>
-                    <Text>Memo App</Text>
-                    <Text>ログアウト</Text>
+
+            <View style={styles.header}>
+                <View style={styles.headerInner}>
+                    <Text style={styles.headerTitle}>Memo App</Text>
+                    <Text style={styles.headerRight}>ログアウト</Text>
                 </View>
             </View>
 
             <View> {/* memo list */}
-                <View> {/* memo list item */}
+                <View style={styles.memoListItem}> {/* memo list item */}
                     <View> {/* 左 */}
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
                     </View>
                     <View> {/* 右 */}
                         <Text>X</Text>
@@ -23,10 +24,10 @@ const Index = (): JSX.Element => {
             </View>
 
             <View> {/* memo list */}
-                <View> {/* memo list item */}
+                <View style={styles.memoListItem}> {/* memo list item */}
                     <View> {/* 左 */}
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
                     </View>
                     <View> {/* 右 */}
                         <Text>X</Text>
@@ -35,10 +36,10 @@ const Index = (): JSX.Element => {
             </View>
 
             <View> {/* memo list */}
-                <View> {/* memo list item */}
+                <View style={styles.memoListItem}> {/* memo list item */}
                     <View> {/* 左 */}
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+                        <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
                     </View>
                     <View> {/* 右 */}
                         <Text>X</Text>
@@ -46,8 +47,8 @@ const Index = (): JSX.Element => {
                 </View>
             </View>
 
-            <View>
-                <Text>+</Text>
+            <View style={styles.circleButton}>
+                <Text style={styles.circleButtonLabel}>+</Text>
             </View>
 
         </View>
@@ -57,9 +58,61 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        backgroundColor: '#ffffff'
+    },
+    header: {
+        backgroundColor: '#467FD3',
+        height: 104,
+        justifyContent: 'flex-end'
+    },
+    headerInner: {
         alignItems: 'center'
+    },
+    headerRight: { 
+        position: 'absolute',
+        right: 16,
+        bottom: 16,
+        color: 'rgba(255,255,255,0.7)'
+    },
+    headerTitle: { 
+        marginBottom: 8,
+        fontSize: 22,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+    memoListItem:{
+        backgroundColor: '#ffffff',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+        paddingHorizontal: 19,
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: 'rgba(0,0,0,0.15)'
+    },
+    memoListItemTitle:{
+        fontSize: 16,
+        lineHeight: 32
+    },
+    memoListItemDate:{
+        fontSize: 12,
+        lineHeight: 16,
+        color: '#848484'
+    },
+    circleButton:{
+        width: 64,
+        height: 64,
+        borderRadius:32,
+        backgroundColor: '#467FD3',
+        justifyContent: 'center',
+        // alignItems: 'center'
+    },
+    circleButtonLabel:{
+
     }
+
+
 })
 
 export default Index
